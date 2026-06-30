4f6e11272a813b8da7987807554325b9d280420c
K-Sqr <emmanueladecoder@gmail.com>
2026-06-29 19:20:14 -0500

    Add Vercel Web Analytics to both calculator and diagnostic entries

--- src/diagnostic/main.tsx
+++ src/diagnostic/main.tsx
@@ -1,10 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { Analytics } from "@vercel/analytics/react";
 import DiagnosticApp from "./DiagnosticApp";
 import "../index.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <DiagnosticApp />
+    <Analytics />
   </React.StrictMode>
 );
